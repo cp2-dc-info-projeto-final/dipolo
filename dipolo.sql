d88940ac73bf71c32407dcf74154e1eb7a3c86fd
Louise F. Silva <82733024+Louise062721@users.noreply.github.com>
2021-10-28 09:48:29 -0300

    Arquivo de comandos do banco de dados

---
+++ dipolo.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE tccfinal3;
+
+CREATE TABLE usuarios (
+ cod_usuario int NOT NULL AUTO_INCREMENT, 
+ nickname varchar(10) NOT NULL,
+ nome varchar(100) NOT NULL,
+ datanasc date NOT NULL,
+ email varchar(35) NOT NULL,
+ senha varchar(255) NOT NULL,
+ adm char(3) NOT NULL,
+ primary key(cod_usuario)
+);
+
+CREATE USER 'person'@'localhost' IDENTIFIED BY '1010';
+GRANT ALL PRIVILEGES ON tccfinal3.* TO 'person'@'localhost';
+
