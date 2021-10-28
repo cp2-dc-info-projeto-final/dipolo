fb47f7b4ace5ac778c85c158124242d574ad1bee
Felipe Marins <felipemarins555@gmail.com>
2021-10-28 19:50:29 -0300

    Mover arquivos de códigos para a pasta src

--- src/index.php
+++ src/index.php
@@ -1,8 +1,23 @@
+<?php include "autentica.inc"; ?>
+
 <html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-   <?php echo "Hello World!";?>
- </body>
+    <head>
+        <title>Página inicial</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        Texto Aleatório
+        <p><a href="logout.php">LOGOUT</a></p>
+        <br><br>
+
+        <p><strong>Exibir Meus Dados</strong></p>
+        <form action="recebe_dados.php" method="POST">
+            <input type="hidden" name="login" value="exibir">
+            <input type="hidden" name="nick" value="<?php echo $_SESSION["nickname"] ?>">
+            <p><input type="submit" value="Exibir"></p>
+        </form>
+
+
+    </body>
 </html>
