5ca531e7634142cebdddab51a264a28769a31104
Felipe Marins <felipemarins555@gmail.com>
2021-10-30 15:29:51 -0300

    Move o footer para um arquivo separado

--- src/cadastro.php
+++ src/cadastro.php
@@ -1,72 +1,42 @@
 <html>
-    <head>
-        <title>Cadastrar Usuário</title>
-        <meta charset="utf-8">
-        <link href="css/main.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </head>
 
-    <body>
-        <p><strong>FORMUÁRIO DE CADASTRO</strong></p>
-        <form action="recebe_dados.php" method="POST">
-            <input type="hidden" name="login" value="cadastrar">
+<head>
+    <title>Cadastrar Usuário</title>
+    <meta charset="utf-8">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
 
-            <p>Nickname: <input type="text" name="nickname" size="10" maxlength="10" required></p>
-        
-            <p>Nome Completo: <input type="text" name="nome" size="30" maxlength="100" required></p>
+<body>
+    <p><strong>FORMUÁRIO DE CADASTRO</strong></p>
+    <form action="recebe_dados.php" method="POST">
+        <input type="hidden" name="login" value="cadastrar">
 
-            <p>Data de Nascimento: <input type="date" name="datanasc" required></p>
+        <p>Nickname: <input type="text" name="nickname" size="10" maxlength="10" required></p>
 
-            <p>Email: <input type="text" name="email" size="30" maxlength="35" required></p>
+        <p>Nome Completo: <input type="text" name="nome" size="30" maxlength="100" required></p>
 
-            <p>Confirmar Email: <input type="text" name="confemail" size="30" maxlength="35" required></p>
+        <p>Data de Nascimento: <input type="date" name="datanasc" required></p>
 
-            <p>Senha: <input type="password" name="senha" maxlength="12" required></p>
+        <p>Email: <input type="text" name="email" size="30" maxlength="35" required></p>
 
-            <p>Confirmar Senha: <input type="password" name="confsenha" maxlength="12" required></p>
+        <p>Confirmar Email: <input type="text" name="confemail" size="30" maxlength="35" required></p>
 
-            <p>Se for um administrador, insira seu código <br>
+        <p>Senha: <input type="password" name="senha" maxlength="12" required></p>
+
+        <p>Confirmar Senha: <input type="password" name="confsenha" maxlength="12" required></p>
+
+        <p>Se for um administrador, insira seu código <br>
             Código de Administrador: <input type="password" name="codadm"></p>
- 
-            <p><input type="submit" value="Enviar"></p>
-        </form>
 
-        <!-- Footer -->
+        <p><input type="submit" value="Enviar"></p>
+    </form>
 
-    <footer class="bg-azul text-light">
-        <div class="container-fluid text-md-left pt-4">
-            <div class="row justify-content-start mb-3">
-                <div class="col-md-2 mt-md-0 mt-n2">
-                    <a class="text-light text-decoration-none fs-1 ms-4" style="font-weight: 600;" href="index.php">Dipolo</a>
-                </div>
-                <hr class="clearfix w-100 d-md-none pb-3">
-                <div class="col-md-2 mb-md-0 mb-3 pt-3">
-                    <ul class="list-unstyled">
-                        <li class="mb-2">
-                            <a class="text-light text-decoration-none" href="index.php">Página Principal</a>
-                        </li>
-                        <li class="mb-2">
-                            <a class="text-light text-decoration-none" href="#!">Ajuda</a>
-                        </li>
-                        <li class="mb-2">
-                            <a class="text-light text-decoration-none" href="#!">Sobre</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-2 mb-md-0 mb-3 pt-3">
-                    <ul class="list-unstyled">
-                        <li class="mb-2">
-                            <a class="text-light text-decoration-none" href="#!">Enviar Feedback</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="text-center py-3">
-            <a>Dipolo Corporation © 2021</a>
-        </div>
-    </footer>
+    <!-- Footer -->
 
-    </body>
+    <?php echo file_get_contents('footer.html') ?>
+
+</body>
+
 </html>
