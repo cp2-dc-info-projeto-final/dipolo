62141208043b9f6d30a6b9351b30325752034600
Felipe Marins <felipemarins555@gmail.com>
2021-10-30 18:03:45 -0300

    Organiza página de login

--- src/login.php
+++ src/login.php
@@ -4,11 +4,8 @@
     <title>Login</title>
     <meta charset="utf-8">
     <link href="css/main.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </head>
 
@@ -24,19 +21,27 @@
 
     <!-- Conteúdo -->
 
-    <p><strong>LOGIN</strong></p>
-    <form action="efetuar_login.php" method="POST">
+    <div class="container-flex p-5 py-5">
+        <div class="row justify-content-between">
+            <div class="col-3">
+                <h1 class="text-center mb-4">Entrar</h1>
+                <form action="efetuar_login.php" method="POST">
+                    <div class="mb-3">
+                        <label for="entrarInputNickname" class="form-label">Nickname</label>
+                        <input type="text" class="form-control" id="entrarInputNickname" name="nickname" size="11" maxlength="10" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="entrarInputPassword" class="form-label">Senha</label>
+                        <input type="password" class="form-control" id="entrarInputPassword" name="senha" size="15" maxlength="12" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+            <div class="col-3">
 
-        <p>Nickname: <input type="text" name="nickname" size="11" maxlength="10" required></p>
-
-        <p>Senha: <input type="password" name="senha" size="15" maxlength="12" required></p>
-
-        <p><input type="submit" value="Enviar"></p>
-    </form>
-
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;OU</p>
-    <a href="cadastro.php"><strong>CADASTRAR-SE</strong></a>
+            </div>
+        </div>
+    </div>
 
     <!-- Footer -->
 
