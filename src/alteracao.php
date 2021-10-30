ec78a77c6418ef199cdbff0d218c0190e6e6f25c
Felipe Marins <felipemarins555@gmail.com>
2021-10-30 16:06:24 -0300

    Move a navbar para um arquivo separado

--- src/alteracao.php
+++ src/alteracao.php
@@ -21,28 +21,17 @@ $usuario = mysqli_fetch_array($resposta);
 </head>
 
 <body>
-    <nav class="navbar d-block navbar-dark bg-azul">
-        <div class="d-flex">
-            <button type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="index.php">Página principal</a></li>
-                <li><a class="dropdown-item" href="#">Entrar / Cadastrar</a></li>
-                <li><a class="dropdown-item" href="#">Ajuda</a></li>
-                <li><a class="dropdown-item" href="#">Enviar feedback</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Sobre</a></li>
-            </ul>
-            <a class="navbar-brand ms-2" style="font-weight: 500;" href="#">Dipolo</a>
-            <form class="d-flex ms-auto my-2 my-lg-0 me-4">
-                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-            </form>
-        </div>
-    </nav>
+
+    <!-- Navbar -->
+
+    <?php
+    ob_start();
+    include 'navbar.php';
+    echo ob_get_clean();
+    ?>
+
+    <!-- Conteúdo -->
+
     <p><strong>Atualização de Dados</strong></p>
     <form action="recebe_dados.php" method="POST">
         <input type="hidden" name="login" value="alterar">
--- src/login.php
+++ src/login.php
@@ -16,28 +16,14 @@
 
     <!-- Navbar -->
 
-    <nav class="navbar d-block navbar-dark bg-azul">
-        <div class="d-flex">
-            <button type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="index.php">Página principal</a></li>
-                <li><a class="dropdown-item" href="#">Entrar / Cadastrar</a></li>
-                <li><a class="dropdown-item" href="#">Ajuda</a></li>
-                <li><a class="dropdown-item" href="#">Enviar feedback</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Sobre</a></li>
-            </ul>
-            <a class="navbar-brand ms-2" style="font-weight: 500;" href="#">Dipolo</a>
-            <form class="d-flex ms-auto my-2 my-lg-0 me-4">
-                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-            </form>
-        </div>
-    </nav>
+    <?php
+    ob_start();
+    include 'navbar.php';
+    echo ob_get_clean();
+    ?>
+
+    <!-- Conteúdo -->
+
     <p><strong>LOGIN</strong></p>
     <form action="efetuar_login.php" method="POST">
 
