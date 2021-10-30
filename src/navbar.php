741f2d9ada89daf9d602da2157c72fdcc4445a3f
Felipe Marins <felipemarins555@gmail.com>
2021-10-30 19:13:12 -0300

    Especifica páginas e funções não implementadas

--- src/navbar.php
+++ src/navbar.php
@@ -6,17 +6,17 @@
         <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="index.php">Página principal</a></li>
             <li><a class="dropdown-item" href="login_cadastro.php">Entrar / Cadastrar</a></li>
-            <li><a class="dropdown-item" href="#">Ajuda</a></li>
-            <li><a class="dropdown-item" href="#">Enviar feedback</a></li>
+            <li><a class="dropdown-item disabled" href="#">Ajuda</a></li>
+            <li><a class="dropdown-item disabled" href="#">Enviar feedback</a></li>
             <li>
                 <hr class="dropdown-divider">
             </li>
-            <li><a class="dropdown-item" href="#">Sobre</a></li>
+            <li><a class="dropdown-item disabled" href="#">Sobre</a></li>
         </ul>
         <a class="navbar-brand ms-2" style="font-weight: 500;" href="index.php">Dipolo</a>
         <form class="d-flex ms-auto my-2 my-lg-0 me-4">
-            <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+            <input class="form-control me-2" type="search" placeholder="A ser implementada" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit" disabled>Pesquisar</button>
         </form>
     </div>
 </nav>
