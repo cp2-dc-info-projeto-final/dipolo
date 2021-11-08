78be44f489c1f16c49a1a650e38873a8d0b40c46
Louise F. Silva <82733024+Louise062721@users.noreply.github.com>
2021-11-08 19:33:07 -0300

    Update recebe_dados.php

--- src/recebe_dados.php
+++ src/recebe_dados.php
@@ -37,6 +37,32 @@
         echo "<br><Br><a href='index.php'> Voltar para tela inicial </a>";
     }
 
+    if($login == "buscar_usuarios")
+    {
+        $nick = $_POST["nick"];
+        $nick = htmlspecialchars($nick);
+                
+        $sql ="SELECT * FROM usuarios WHERE nickname LIKE '%$nick%';"; 
+        $resposta = mysqli_query($mysqli,$sql);
+        $linhas = mysqli_num_rows($resposta);
+
+        if($linhas == 0)
+        {
+            echo "Nenhum usuário foi encontrado";
+        }
+
+        for($i = 0; $i < $linhas; $i++)
+        {
+            $usuario = mysqli_fetch_array($resposta);
+            echo $usuario["nickname"]."<br><br>";
+            //echo "<a href='index.php'>". $usuario["nickname"] . "</a> <br>";
+            //colocar nickname já como link para a página do usuário buscado?
+            //aparecer as opções de usuários em um dropdown?
+        }
+          
+        echo "<br><Br><a href='index.php'> Voltar para tela inicial </a>";
+    }
+
     mysqli_close($mysqli);
 
 ?>
