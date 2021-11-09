8a45ec71e3ba935f2067135e60875e55dca9f1ba
Louise F. Silva <82733024+Louise062721@users.noreply.github.com>
2021-11-09 13:10:57 -0300

    Update login_cadastro.php

--- src/login_cadastro.php
+++ src/login_cadastro.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>Login</title>
+    <title>Cadastro</title>
     <meta charset="utf-8">
     <link href="css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -72,23 +72,6 @@
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </form>
-            </div>
-
-            <!-- Coluna com formulário de autenticação -->
-
-            <div class="col-3">
-                <h1 class="text-center mb-4">Entrar</h1>
-                <form action="efetuar_login.php" method="POST">
-                    <div class="mb-3">
-                        <label for="entrarInputNickname" class="form-label">Nickname</label>
-                        <input type="text" class="form-control" id="entrarInputNickname" name="nickname" size="11" maxlength="10" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="entrarInputPassword" class="form-label">Senha</label>
-                        <input type="password" class="form-control" id="entrarInputPassword" name="senha" size="15" maxlength="12" required>
-                    </div>
                     <button type="submit" class="btn btn-primary">Enviar</button>
                 </form>
             </div>
