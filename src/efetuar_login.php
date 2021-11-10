3accf1ff03d3c6c5a805e8f6586ef867e126804e
Louise F. Silva <82733024+Louise062721@users.noreply.github.com>
2021-11-10 11:35:08 -0300

    Update efetuar_login.php

--- src/efetuar_login.php
+++ src/efetuar_login.php
@@ -10,14 +10,14 @@
 
     if(mysqli_num_rows($resposta) != 1){ // testa se não encontrou o nickname
         echo "Nickname inválido!";
-        echo "<p><a href='login_cadastro.php'>Página de login</a></p>";
+        echo "<p><a href='index.php'>Página de login</a></p>";
     }
     
     else{
         $usuario = mysqli_fetch_array($resposta);
         if(!password_verify($senha, $usuario["senha"])){ // testa se a senha está errada
             echo "Senha inválida!";
-            echo "<p><a href='login_cadastro.php'>Página de login</a></p>";
+            echo "<p><a href='index.php'>Página de login</a></p>";
         }
         else{ // usuário e senha corretos, abre a sessão
             session_start();
