4de359442b8577d943032bdcac287da2f5aa96c8
Louise062721 <louisefcos10@gmail.com>
2021-11-11 23:35:48 -0300

    Update barras_de_busca.php

--- src/barras_de_busca.php
+++ src/barras_de_busca.php
@@ -1,5 +1,6 @@
 <!--Página temporária-->
 <!--Barras de busca a serem inseridas na página do administrador-->
+<!--Página temporária-->
 
 <form action="recebe_busca.php" method="POST" class="d-flex ms-auto my-2 my-lg-0 me-4">
     <input type="hidden" name="login" value="buscar_usuarios">
