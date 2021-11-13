578f997dbc1459315313aff8f4f245792d1caed5
Felipe Marins <felipemarins555@gmail.com>
2021-11-13 15:05:31 -0300

    Mudar o link "Editar" para um ícone

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@
     <link href="css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -81,7 +82,10 @@
                     <div class="col-2">
                         <div class="figure-img img-fluid rounded px-5 py-5 bg-azul text-light">Imagem de perfil (A ser implementada)</div>
                         <p class="text-center fs-4 mb-0">
-                            <?php echo return_dados("nickname"); ?> <a class="text-decoration-none fw-bold" href="alteracao.php?cod_usuario=<?php echo return_dados("cod_usuario"); ?>">(Editar)</a>
+                            <?php echo return_dados("nickname"); ?>
+                            <a class="text-decoration-none text-dark" href="alteracao.php?cod_usuario=<?php echo return_dados("cod_usuario"); ?>">
+                                <i class="bi bi-gear" role="img" aria-label="Editar dados"></i>
+                            </a>
                         </p>
                         <p class="text-center fw-light text-muted"><?php echo return_dados("nome"); ?></p>
                         <p class="">Bio (A ser implementada)</p>
