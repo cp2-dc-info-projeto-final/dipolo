939f0e5c374a9d34a0d846c4065e068309bc418b
Louise062721 <louisefcos10@gmail.com>
2021-11-24 19:29:08 -0300

    Código para o usuário excluir sua própria conta

--- src/excluir_conta.php
+++ src/excluir_conta.php
@@ -14,3 +14,14 @@ if ($_SESSION["fez_login"]) {
         header("Location: admin_dashboard.php");
     }
 }
+
+$login = $_POST["login"];
+
+if($login == "exclui_conta")
+{
+    $cod_usuario = $_POST["cod_usuario"];
+
+    $sql ="DELETE FROM usuarios WHERE cod_usuario = $cod_usuario;"; 
+    mysqli_query($mysqli,$sql);
+    header("Location: index.php");
+}
