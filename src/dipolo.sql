be3ef7978cf9d837c52e3bad9b50b6757fbc0390
Louise062721 <louisefcos10@gmail.com>
2021-11-25 11:08:26 -0300

    Criação das tabelas postagens, cometarios e timeline

--- src/dipolo.sql
+++ src/dipolo.sql
@@ -18,7 +18,35 @@ CREATE TABLE usuarios (
  primary key(cod_usuario)
 );
 
-INSERT INTO `usuarios` (`cod_usuario`, `nickname`, `nome`, `datanasc`, `email`, `senha`, `adm`) 
-VALUES (NULL, 'MainADM', 'Administrador Principal', '2021-11-11', 'adm.principal@gmail.com', 
+INSERT INTO usuarios ('cod_usuario', 'nickname', 'nome', 'datanasc', 'email', 'senha', 'adm') 
+VALUES (NULL, 'MainADM', 'Administrador Principal', '2000-11-11', 'adm.principal@gmail.com', 
 '$2y$10$ekt1zZWnWT3XG4HDTzdlUuZIcxgnKvr3OiZZ0e76HORSSBc5dWtZi', '1');
 
+CREATE TABLE postagens (
+ cod_postagem int NOT NULL AUTO_INCREMENT,
+ texto_post varchar(350) NOT NULL,
+ cod_usuario int NOT NULL,
+ cod_comentario int NOT NULL,
+ primary key(cod_postagem),
+ foreign key (cod_usuario) references usuarios (cod_usuario)
+);
+
+CREATE TABLE comentarios (
+ cod_comentario int NOT NULL AUTO_INCREMENT,
+ texto_coment varchar(350) NOT NULL,
+ cod_usuario int NOT NULL,
+ cod_postagem int NOT NULL,
+ primary key(cod_comentario),
+ foreign key (cod_usuario) references usuarios (cod_usuario),
+ foreign key (cod_postagem) references postagens (cod_postagem)
+);
+
+CREATE TABLE timeline (
+ cod_usuario int NOT NULL,
+ cod_postagem int NOT NULL,
+ primary key(cod_usuario, cod_postagem),
+ foreign key (cod_usuario) references usuarios (cod_usuario),
+ foreign key (cod_postagem) references postagens (cod_postagem)
+);
+
+ALTER TABLE postagens ADD foreign key (cod_comentario) references comentarios (cod_comentario);
