42f5f4da9953240398a5137867e018fccd1e0262
Louise062721 <louisefcos10@gmail.com>
2021-11-26 16:27:47 -0300

    Correção do nome do botão

--- src/timeline.php
+++ src/timeline.php
@@ -1,5 +1,6 @@
+<br><Br><br>
 <form action="recebe_timeline.php" method="POST">
     <input type="hidden" name="publi" value="timeline">
     <input type="hidden" name="cod_usuario" value="<?php echo return_dados("cod_usuario", ""); ?>">
-    <button type="submit" class="btn btn-outline-primary btn-lg">Criar postagem</button>
+    <button type="submit" class="btn btn-outline-primary btn-lg">Ver timeline</button>
 </form>
