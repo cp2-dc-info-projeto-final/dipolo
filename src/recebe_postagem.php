1189c6531c2a3875ea758af1577d2b6346d3598c
Louise062721 <louisefcos10@gmail.com>
2021-11-26 17:12:40 -0300

    Insere os dados na tabela timeline

--- src/recebe_postagem.php
+++ src/recebe_postagem.php
@@ -15,20 +15,22 @@ if($publi == "post")
     $sql .= "VALUES (NULL, '$texto_post', $cod_usuario);";
     mysqli_query($mysqli,$sql);
 
+    header("Location: index.php");
+}
+
     $sql2 = "SELECT * FROM postagens";
-    $resposta = mysqli_query($mysqli,$sql2);
+    $resposta = mysqli_query($mysqli, $sql2);
     $linhas = mysqli_num_rows($resposta);
 
     for($i = 0; $i < $linhas; $i++)
     {
         $post = mysqli_fetch_array($resposta);
         $cod_postagem = $post["cod_postagem"];
+        $cod_usuario = $post["cod_usuario"];
 
-        $sql3 = "INSERT INTO timeline (cod_usuario, cod_postagem)" ;
-        $sql3 .= "VALUES ($cod_usuario, $cod_postagem";
+        $sql1 = "INSERT INTO timeline (cod_usuario, cod_postagem)" ;
+        $sql1 .= "VALUES ($cod_usuario, $cod_postagem);";
+        mysqli_query($mysqli,$sql1);
     }
-
-    header("Location: index.php");
-}
 
 ?>
