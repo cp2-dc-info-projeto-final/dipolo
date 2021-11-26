5f79d24638959c2a66c83aae17f475da4e7665ea
Louise062721 <louisefcos10@gmail.com>
2021-11-26 15:57:09 -0300

    Estrutura básica da timeline

---
+++ src/timeline.php
@@ -0,0 +1,5 @@
+<form action="recebe_timeline.php" method="POST">
+    <input type="hidden" name="publi" value="timeline">
+    <input type="hidden" name="cod_usuario" value="<?php echo return_dados("cod_usuario", ""); ?>">
+    <button type="submit" class="btn btn-outline-primary btn-lg">Criar postagem</button>
+</form>
