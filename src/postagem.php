2d7d4f7ca792c72c0ed48f03f2b31f51541c5afa
Louise062721 <louisefcos10@gmail.com>
2021-11-26 10:59:07 -0300

    Correção

--- src/postagem.php
+++ src/postagem.php
@@ -2,7 +2,7 @@
     <form action="recebe_postagem.php" method="POST">
         <input type="hidden" name="publi" value="post">
         <input type="hidden" name="cod_usuario" value="<?php echo return_dados("cod_usuario", ""); ?>">
-        <textarea class="form-control mb-3" name="texto_post" rows="5" placeholder="Comece seu debate aqui" required></textarea>
+        <textarea class="form-control mb-3" name="texto_post" rows="5" maxlength="350" placeholder="Comece seu debate aqui" required></textarea>
         <button type="submit" class="btn btn-outline-primary btn-lg">Criar postagem</button>
     </form>
 </div>
