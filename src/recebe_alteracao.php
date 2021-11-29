6179391e74551ba9033bd4671404bd65e53b7fc0
Louise062721 <louisefcos10@gmail.com>
2021-11-28 21:02:03 -0300

    Estrutura funcional para editar postagem

--- src/recebe_alteracao.php
+++ src/recebe_alteracao.php
@@ -158,7 +158,46 @@
 
     if($login == "alterar_postagem")
     {
-        
+        $texto_post = $_POST["texto_post"];
+        $cod_postagem = $_POST["cod_postagem"];
+        $erro = 0;
+        $texto_post = htmlspecialchars($texto_post);
+
+        if(empty($texto_post))
+        {
+            echo "Insira seu argumento <br>";
+            $erro = 1;
+        }
+
+        /*if(trim($texto_post))
+        {
+            echo "Insira seu argumento <br>";
+            $erro = 1;
+        }*/
+
+        if($erro == 0)
+        {
+            $sql ="UPDATE postagens SET texto_post = '$texto_post'"; 
+            $sql .= "WHERE cod_postagem = $cod_postagem;";
+            mysqli_query($mysqli,$sql);
+
+            /*$sql2 = "SELECT * FROM postagens";
+            $resposta = mysqli_query($mysqli, $sql2);
+            $linhas = mysqli_num_rows($resposta);
+
+            for($i = 0; $i < $linhas; $i++)
+            {
+                $post = mysqli_fetch_array($resposta);
+                $cod_postagem = $post["cod_postagem"];
+                $cod_usuario = $post["cod_usuario"];
+
+                $sql1 = "INSERT INTO timeline (cod_usuario, cod_postagem)" ;
+                $sql1 .= "VALUES ($cod_usuario, $cod_postagem);";
+                mysqli_query($mysqli,$sql1);
+            }*/
+        }
+
+        header("Location: index.php");
     }
 
     
