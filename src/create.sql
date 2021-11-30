777377e3782bbe4c7b905b20f4fc8e1c80626209
Felipe Marins <felipemarins555@gmail.com>
2021-11-29 21:24:07 -0300

    Atualiza create.sql

--- src/create.sql
+++ src/create.sql
@@ -1,52 +1,173 @@
-DROP DATABASE IF EXISTS dipolotcc;
-DROP USER IF EXISTS 'dipolo'@'localhost';
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 30-Nov-2021 às 00:22
+-- Versão do servidor: 10.6.5-MariaDB
+-- versão do PHP: 7.4.25
 
-CREATE DATABASE dipolotcc;
-CREATE USER 'dipolo'@'localhost' IDENTIFIED BY '1010';
-GRANT ALL PRIVILEGES ON dipolotcc.* TO 'dipolo'@'localhost';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-USE dipolotcc;
 
-CREATE TABLE usuarios (
- cod_usuario int NOT NULL AUTO_INCREMENT, 
- nickname varchar(10) NOT NULL,
- nome varchar(100) NOT NULL,
- datanasc date NOT NULL,
- email varchar(35) NOT NULL,
- senha varchar(255) NOT NULL,
- adm boolean NOT NULL,
- primary key(cod_usuario)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO usuarios ('cod_usuario', 'nickname', 'nome', 'datanasc', 'email', 'senha', 'adm') 
-VALUES (NULL, 'MainADM', 'Administrador Principal', '2000-11-11', 'adm.principal@gmail.com', 
-'$2y$10$ekt1zZWnWT3XG4HDTzdlUuZIcxgnKvr3OiZZ0e76HORSSBc5dWtZi', '1');
+--
+-- Banco de dados: `dipolotcc`
+--
+DROP DATABASE IF EXISTS `dipolotcc`;
+CREATE DATABASE IF NOT EXISTS `dipolotcc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `dipolotcc`;
 
-CREATE TABLE postagens (
- cod_postagem int NOT NULL AUTO_INCREMENT,
- texto_post varchar(350) NOT NULL,
- cod_usuario int NOT NULL,
- cod_comentario int NOT NULL,
- primary key(cod_postagem),
- foreign key (cod_usuario) references usuarios (cod_usuario)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE comentarios (
- cod_comentario int NOT NULL AUTO_INCREMENT,
- texto_coment varchar(350) NOT NULL,
- cod_usuario int NOT NULL,
- cod_postagem int NOT NULL,
- primary key(cod_comentario),
- foreign key (cod_usuario) references usuarios (cod_usuario),
- foreign key (cod_postagem) references postagens (cod_postagem)
-);
+--
+-- Estrutura da tabela `comentarios`
+--
 
-CREATE TABLE timeline (
- cod_usuario int NOT NULL,
- cod_postagem int NOT NULL,
- primary key(cod_usuario, cod_postagem),
- foreign key (cod_usuario) references usuarios (cod_usuario),
- foreign key (cod_postagem) references postagens (cod_postagem)
-);
+CREATE TABLE `comentarios` (
+  `cod_comentario` int(11) NOT NULL,
+  `texto_coment` varchar(350) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cod_usuario` int(11) NOT NULL,
+  `cod_postagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE postagens ADD foreign key (cod_comentario) references comentarios (cod_comentario);
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postagens`
+--
+
+CREATE TABLE `postagens` (
+  `cod_postagem` int(11) NOT NULL,
+  `texto_post` varchar(350) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cod_usuario` int(11) NOT NULL,
+  `cod_comentario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `timeline`
+--
+
+CREATE TABLE `timeline` (
+  `cod_usuario` int(11) NOT NULL,
+  `cod_postagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `cod_usuario` int(11) NOT NULL,
+  `nickname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `datanasc` date NOT NULL,
+  `email` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adm` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`cod_usuario`, `nickname`, `nome`, `datanasc`, `email`, `senha`, `adm`) VALUES
+(1, 'testadm', 'Usuário Admin', '2021-11-29', 'email@example.com', '$2y$10$hDGwTyvP.tJGbShpjv4KyOEyIqlq3ERpuDbWZ94Qmfv2QgtAKU2PS', 1),
+(2, 'test1', 'Usuário Comum', '2021-11-29', 'email@example.com', '$2y$10$5QotISSBV9FxJjH2bHlFLO9Z1C0S2UUKLurv3z53uN7hdMghUm7Ee', 0),
+(3, 'test2', 'Usuário Comum II', '2021-11-29', 'email@example.com', '$2y$10$j00Rut4fH7MDoyYU.oELIe6zXNeSelpgjBaz8eowSm1LJU1MKg9HO', 0),
+(4, 'test3', 'Usuário Comum III', '2021-11-29', 'email@example.com', '$2y$10$WAOVO2Lb6XcFQQFH4HqUWO6WyODkJewKEv44KoOgYbpJXcnr5tNfO', 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`cod_comentario`),
+  ADD KEY `cod_usuario` (`cod_usuario`),
+  ADD KEY `cod_postagem` (`cod_postagem`);
+
+--
+-- Índices para tabela `postagens`
+--
+ALTER TABLE `postagens`
+  ADD PRIMARY KEY (`cod_postagem`),
+  ADD KEY `cod_usuario` (`cod_usuario`),
+  ADD KEY `cod_comentario` (`cod_comentario`);
+
+--
+-- Índices para tabela `timeline`
+--
+ALTER TABLE `timeline`
+  ADD PRIMARY KEY (`cod_usuario`,`cod_postagem`),
+  ADD KEY `cod_postagem` (`cod_postagem`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`cod_usuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `cod_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `postagens`
+--
+ALTER TABLE `postagens`
+  MODIFY `cod_postagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuarios` (`cod_usuario`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`cod_postagem`) REFERENCES `postagens` (`cod_postagem`);
+
+--
+-- Limitadores para a tabela `postagens`
+--
+ALTER TABLE `postagens`
+  ADD CONSTRAINT `postagens_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuarios` (`cod_usuario`),
+  ADD CONSTRAINT `postagens_ibfk_2` FOREIGN KEY (`cod_comentario`) REFERENCES `comentarios` (`cod_comentario`);
+
+--
+-- Limitadores para a tabela `timeline`
+--
+ALTER TABLE `timeline`
+  ADD CONSTRAINT `timeline_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuarios` (`cod_usuario`),
+  ADD CONSTRAINT `timeline_ibfk_2` FOREIGN KEY (`cod_postagem`) REFERENCES `postagens` (`cod_postagem`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
