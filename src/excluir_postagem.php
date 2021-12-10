e24149f6f066a482f69928401a956565a1e64c2d
Louise062721 <louisefcos10@gmail.com>
2021-12-10 17:36:42 -0300

    Correção

--- src/excluir_postagem.php
+++ src/excluir_postagem.php
@@ -3,6 +3,8 @@
     include "conecta_mysql.php";
 
     $cod_postagem = $_GET["cod_postagem"];
+    $sql3 ="DELETE FROM comentarios WHERE cod_postagem = $cod_postagem;"; 
+    mysqli_query($mysqli,$sql3);
     $sql2 ="DELETE FROM timeline WHERE cod_postagem = $cod_postagem;"; 
     mysqli_query($mysqli,$sql2);
     $sql ="DELETE FROM postagens WHERE cod_postagem = $cod_postagem;"; 
