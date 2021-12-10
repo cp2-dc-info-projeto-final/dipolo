6c36bc913f72e51ab57f23e24423d3e4091d02d7
Louise062721 <louisefcos10@gmail.com>
2021-12-10 17:33:07 -0300

    Volta pro commit anterior

--- src/excluir_postagem.php
+++ src/excluir_postagem.php
@@ -2,7 +2,7 @@
 
     include "conecta_mysql.php";
 
-    $cod_postagem = $_POST["cod_postagem"];
+    $cod_postagem = $_GET["cod_postagem"];
     $sql2 ="DELETE FROM timeline WHERE cod_postagem = $cod_postagem;"; 
     mysqli_query($mysqli,$sql2);
     $sql ="DELETE FROM postagens WHERE cod_postagem = $cod_postagem;"; 
--- src/recebe_timeline.php
+++ src/recebe_timeline.php
@@ -52,33 +52,11 @@
 
             if($_SESSION["nickname"] == $nick)
             {
-                echo "<a href='alteracao_postagem.php?cod_postagem=".$cod_postagem.
+                echo "<a class='text-decoration-none text-dark' 
+                        href='alteracao_postagem.php?cod_postagem=".$cod_postagem.
                         "'> Editar postagem </a> <br>";
-
-                echo "<link href='css/main.css' rel='stylesheet'>
-                    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
-                    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p' crossorigin='anonymous'></script>
-                    
-                        <button type= button' data-bs-toggle='modal' data-bs-target='#excluirModalPost'>
-                                Excluir postagem
-                        </button><br>
-                        
-                        <div class='modal fade' id='excluirModalPost' tabindex='-1'aria-labelledby='excluirModalLabelPost' aria-hidden='true'>
-                            <div class='modal-dialog modal-dialog-centered'>
-                                <div class='modal-content'>
-                                    <div class='modal-header'>
-                                        <h4 class='modal-title' id='excluirModalLabelPost'>Confirmar exclusão?</h4>
-                                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Fechar'></button>
-                                    </div>
-                                    <div class='modal-body'>
-                                        <form action='excluir_postagem.php' method='POST'>
-                                            <input type='hidden' name='cod_postagem' value='$cod_postagem'>
-                                            <button type='submit' class='btn btn-primary'>Excluir postagem</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> ";
+                echo "<a href='excluir_postagem.php?cod_postagem=".$cod_postagem.
+                        "'> Excluir postagem </a> <br>";
             }
 
             echo "<a href='comentario.php?cod_postagem=".$cod_postagem.
