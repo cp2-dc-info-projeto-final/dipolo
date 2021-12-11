25004f52dad474963af6a1a333720336d4f14b1c
Felipe Marins <felipemarins555@gmail.com>
2021-12-11 17:47:34 -0300

    Cria função return_postagem

---
+++ src/return_postagem.php
@@ -0,0 +1,11 @@
+<?php
+
+function return_postagem($cod_postagem)
+{
+    include "conecta_mysql.php";
+
+    $sql_postagens = "SELECT * FROM postagens WHERE cod_postagem = $cod_postagem";
+    $resposta_postagens = mysqli_query($mysqli, $sql_postagens);
+    $postagem = mysqli_fetch_array($resposta_postagens);
+    return $postagem;
+}
