95ca3d6dd9d05c2d5924dcabf5345e74f7f277ec
Felipe Marins <felipemarins555@gmail.com>
2021-12-11 17:25:19 -0300

    Conserta erro em return_usuario

--- src/return_usuario.php
+++ src/return_usuario.php
@@ -4,12 +4,12 @@ function return_usuario($cod_usuario)
 {
     include "conecta_mysql.php";
 
-    if ($nickname == "") {
-        $nickname = $_SESSION["nickname"];
-        $nickname = htmlspecialchars($nickname);
+    if ($cod_usuario == null) {
+        $cod_usuario = $_SESSION["nickname"];
+        $cod_usuario = htmlspecialchars($cod_usuario);
     }
 
-    $sql = "SELECT * FROM usuarios WHERE nickname LIKE '$nickname';";
+    $sql = "SELECT * FROM usuarios WHERE cod_usuario = $cod_usuario;";
     $resposta = mysqli_query($mysqli, $sql);
     $usuario = mysqli_fetch_array($resposta);
 
