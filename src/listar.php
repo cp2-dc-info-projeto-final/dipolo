cb7c8ed24d5eff15bc81f49df1d7da92c75d8ab4
Felipe Marins <felipemarins555@gmail.com>
2021-12-11 00:10:14 -0300

    Conserta erro na timeline

--- src/listar.php
+++ src/listar.php
@@ -4,6 +4,7 @@ function listar_postagens($cod_usuario)
 {
     include "conecta_mysql.php";
     include_once "return_dados.php";
+    include_once "verificar_curtida.php";
 
     $sql_usuario = "SELECT * FROM usuarios WHERE cod_usuario = $cod_usuario";
     $resposta_usuario = mysqli_query($mysqli, $sql_usuario);
@@ -21,14 +22,6 @@ function listar_postagens($cod_usuario)
     for ($i = 0; $i < $linhas_postagens; $i++) {
         $postagem = mysqli_fetch_array($resposta_postagens);
         $cod_postagem = $postagem["cod_postagem"];
-
-        $sql_curtidas = "SELECT * FROM curtidas_postagens WHERE cod_postagem = $cod_postagem";
-        $resposta_curtidas = mysqli_query($mysqli, $sql_curtidas);
-        $numero_curtidas = mysqli_num_rows($resposta_curtidas);
-
-        $sql_curtida_propria = "SELECT * FROM curtidas_postagens WHERE cod_usuario = $cod_usuario AND cod_postagem = $cod_postagem";
-        $resposta_curtida_propria = mysqli_query($mysqli, $sql_curtida_propria);
-        $curtiu = mysqli_num_rows($resposta_curtida_propria);
 
         include "timeline_postagem.php";
     }
--- src/timeline_postagem.php
+++ src/timeline_postagem.php
@@ -1,43 +1,38 @@
-<?php if ($linhas_postagens == 0) : ?>
-    <p>Não existem postagens.</p>
-<?php else : ?>
-    <?php include "verificar_curtida.php"; ?>
-    <div class="row mx-0">
-        <div class="mt-3 p-2 ps-3 border border-dark rounded">
-            <div class="row me-0">
-                <p class="pe-0">
-                    <a href="postagem.php?cod_postagem=<?php echo $postagem["cod_postagem"]; ?>" class="text-decoration-none text-dark">
-                        <?php echo $postagem["texto_post"]; ?>
-                    </a>
-                </p>
+<div class="row mx-0">
+    <div class="mt-3 p-2 ps-3 border border-dark rounded">
+        <div class="row me-0">
+            <p class="pe-0">
+                <a href="postagem.php?cod_postagem=<?php echo $postagem["cod_postagem"]; ?>" class="text-decoration-none text-dark">
+                    <?php echo $postagem["texto_post"]; ?>
+                </a>
+            </p>
+        </div>
+        <div class="row me-0 justify-content-end align-items-center">
+            <div class="col-auto px-1">
+                <a href="curtidas_postagens.php?cod_postagem=<?php echo $postagem["cod_postagem"]; ?>" class="btn btn-lg px-2" aria-label="Curtir postagem">
+                    <?php if (usuario_curtiu_postagem($postagem["cod_postagem"], return_dados("cod_usuario", ""))) : ?>
+                        <i class="bi bi-hand-thumbs-up-fill text-primary"></i>
+                    <?php else : ?>
+                        <i class="bi bi-hand-thumbs-up"></i>
+                    <?php endif; ?>
+                </a>
+                <span><?php echo return_curtidas($postagem["cod_postagem"]); ?></span>
             </div>
-            <div class="row me-0 justify-content-end align-items-center">
+            <?php if ($usuario["nickname"] == $_SESSION["nickname"] || $usuario["adm"]) : ?>
                 <div class="col-auto px-1">
-                    <a href="curtidas_postagens.php?cod_postagem=<?php echo $postagem["cod_postagem"]; ?>" class="btn btn-lg px-2" aria-label="Curtir postagem">
-                        <?php if (usuario_curtiu_postagem($postagem["cod_postagem"], return_dados("cod_usuario", ""))) : ?>
-                            <i class="bi bi-hand-thumbs-up-fill text-primary"></i>
-                        <?php else : ?>
-                            <i class="bi bi-hand-thumbs-up"></i>
-                        <?php endif; ?>
-                    </a>
-                    <span><?php echo return_curtidas($postagem["cod_postagem"]); ?></span>
+                    <button class="btn btn-lg" type="button" data-bs-toggle="modal" data-bs-target="#editar<?php echo $usuario['nickname'] ?>Modal">
+                        <i class="bi bi-gear" aria-label="Editar postagem"></i>
+                    </button>
                 </div>
-                <?php if ($usuario["nickname"] == $_SESSION["nickname"] || $usuario["adm"]) : ?>
-                    <div class="col-auto px-1">
-                        <button class="btn btn-lg" type="button" data-bs-toggle="modal" data-bs-target="#editar<?php echo $usuario['nickname'] ?>Modal">
-                            <i class="bi bi-gear" aria-label="Editar postagem"></i>
-                        </button>
-                    </div>
-                    <div class="col-auto px-1">
-                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#excluirModalPostagem<?php echo $cod_postagem; ?>" aria-label="Excluir postagem">
-                            <i class="bi bi-trash-fill"></i>
-                        </button>
-                    </div>
-                <?php endif; ?>
-            </div>
+                <div class="col-auto px-1">
+                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#excluirModalPostagem<?php echo $cod_postagem; ?>" aria-label="Excluir postagem">
+                        <i class="bi bi-trash-fill"></i>
+                    </button>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
-<?php endif; ?>
+</div>
 
 <div class="modal fade" id="editar<?php echo $postagem['cod_postagem'] ?>Modal" tabindex="-1" aria-labelledby="editar<?php echo $postagem['cod_postagem'] ?>ModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
