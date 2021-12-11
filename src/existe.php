f4453fb4e623f7ef663f89bc7d954c2381e30889
Felipe Marins <felipemarins555@gmail.com>
2021-12-10 22:56:22 -0300

    Cria funções usuario_existe e postagem_existe

---
+++ src/existe.php
@@ -0,0 +1,31 @@
+<?php
+
+function usuario_existe($nickname)
+{
+    include "conecta_mysql.php";
+
+    $sql_usuario = "SELECT * FROM usuarios WHERE nickname LIKE \"$nickname\"";
+    $resposta_usuario = mysqli_query($mysqli, $sql_usuario);
+    $linhas_usuario = mysqli_num_rows($resposta_usuario);
+
+    if ($linhas_usuario == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function postagem_existe($cod_postagem)
+{
+    include "conecta_mysql.php";
+
+    $sql_postagens = "SELECT * FROM postagens WHERE cod_usuario = $cod_usuario";
+    $resposta_postagens = mysqli_query($mysqli, $sql_postagens);
+    $linhas_postagens = mysqli_num_rows($resposta_postagens);
+
+    if ($linhas_postagens == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
