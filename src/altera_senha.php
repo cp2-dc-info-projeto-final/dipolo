565a0c35d579bd62fc960888de2210269c315423
Felipe Marins <felipemarins555@gmail.com>
2021-12-11 15:53:26 -0300

    Implementa favicon e muda título de páginas

--- src/altera_senha.php
+++ src/altera_senha.php
@@ -8,7 +8,8 @@ $cod_usuario = $_GET["cod_usuario"];
 <html>
 
 <head>
-    <title>Alterar Senha</title>
+    <title>Alterar Senha - Dipolo</title>
+    <link rel="icon" type="image/x-icon" href="../imagens/favicon.png">
     <meta charset="utf-8">
     <link href="css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
--- src/alteracao.php
+++ src/alteracao.php
@@ -8,8 +8,9 @@ $cod_usuario = $_GET["cod_usuario"];
 <html>
 
 <head>
-    <title>Alterar Usuário</title>
+    <title>Editar dados - Dipolo</title>
     <meta charset="utf-8">
+    <link rel="icon" type="image/x-icon" href="../imagens/favicon.png">
     <link href="css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
