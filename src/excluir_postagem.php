765e411e43c0bbfd70e44bf89648252ca1d68599
Felipe Marins <felipemarins555@gmail.com>
2021-12-13 23:50:00 -0300

    Corrige erro em excluir_postagem.php

--- src/excluir_postagem.php
+++ src/excluir_postagem.php
@@ -7,9 +7,9 @@
     mysqli_query($mysqli,$sql3);
     $sql2 ="DELETE FROM timeline WHERE cod_postagem = $cod_postagem;";
     mysqli_query($mysqli,$sql2);
-    $sql ="DELETE FROM postagens WHERE cod_postagem = $cod_postagem;";
-    mysqli_query($mysqli,$sql);
     $sql_curtidas ="DELETE FROM curtidas_postagens WHERE cod_postagem = $cod_postagem;";
+    mysqli_query($mysqli,$sql_curtidas);
+    $sql ="DELETE FROM postagens WHERE cod_postagem = $cod_postagem;";
     mysqli_query($mysqli,$sql);
     header(sprintf('location: %s', $_SERVER['HTTP_REFERER']));
 
