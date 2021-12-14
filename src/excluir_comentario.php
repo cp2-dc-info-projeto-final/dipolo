55edd8f9afe11b9d6ec1767be4fe7cd0f8b3268f
Felipe Marins <felipemarins555@gmail.com>
2021-12-13 23:46:51 -0300

    Corrige erro de exclusão de comentário

--- src/excluir_comentario.php
+++ src/excluir_comentario.php
@@ -3,10 +3,10 @@
     include "conecta_mysql.php";
 
     $cod_comentario = $_POST["cod_comentario"];
-    $sql_comentarios ="DELETE FROM comentarios WHERE cod_comentario = $cod_comentario;";
-    mysqli_query($mysqli, $sql_comentarios);
     $sql_curtidas_comentarios = "DELETE FROM curtidas_comentarios WHERE cod_comentario = $cod_comentario;";
     mysqli_query($mysqli, $sql_curtidas_comentarios);
+    $sql_comentarios ="DELETE FROM comentarios WHERE cod_comentario = $cod_comentario;";
+    mysqli_query($mysqli, $sql_comentarios);
     header(sprintf('location: %s', $_SERVER['HTTP_REFERER']));
 
     mysqli_close($mysqli);
