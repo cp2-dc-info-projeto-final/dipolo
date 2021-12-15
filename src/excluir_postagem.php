3324d09e02d4d8936e96ae2fdc1bcc5c6e00f004
Louise062721 <louisefcos10@gmail.com>
2021-12-14 22:12:43 -0300

    Exclui tudo relacionado à postagem a ser excluída

--- src/excluir_postagem.php
+++ src/excluir_postagem.php
@@ -3,12 +3,22 @@
     include "conecta_mysql.php";
 
     $cod_postagem = $_POST["cod_postagem"];
+
+    $sql6 = "SELECT * FROM comentarios WHERE cod_postagem = $cod_postagem";
+    $resp = mysqli_query($mysqli,$sql6);
+    $linhas = mysqli_num_rows($resp);
+    for ($i=0; $i < $linhas; $i++){
+        $coment = mysqli_fetch_array($resp);
+        $cod_comentario = $coment["cod_comentario"];
+        $sql5 ="DELETE FROM curtidas_comentarios WHERE cod_comentario = $cod_comentario;";
+        mysqli_query($mysqli,$sql5);
+    }
+    $sql4 ="DELETE FROM curtidas_postagens WHERE cod_postagem = $cod_postagem;";
+    mysqli_query($mysqli,$sql4);
     $sql3 ="DELETE FROM comentarios WHERE cod_postagem = $cod_postagem;";
     mysqli_query($mysqli,$sql3);
     $sql2 ="DELETE FROM timeline WHERE cod_postagem = $cod_postagem;";
     mysqli_query($mysqli,$sql2);
-    $sql_curtidas ="DELETE FROM curtidas_postagens WHERE cod_postagem = $cod_postagem;";
-    mysqli_query($mysqli,$sql_curtidas);
     $sql ="DELETE FROM postagens WHERE cod_postagem = $cod_postagem;";
     mysqli_query($mysqli,$sql);
     header(sprintf('location: %s', $_SERVER['HTTP_REFERER']));
